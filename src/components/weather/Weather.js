8410deeffb10ad7d119e12de32f59829e1adc616
keipy27 <kevinandris27@gmail.com>
2023-09-01 10:58:31 +1200

    implemented variables with useState and implemented the api with url and its key

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,35 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Weather.scss"
 
 const Weather = () => {
-  return (
-    <section className='--100vh --center-all'>
-        <div className="container weather --flex-center">
 
-            <div className="weather-app --text-light">
+    /* variables declaration with useState */
+    const [input, setInput] = useState("")
+    const [errorMsg, setErrorMsg] = useState("")
+    const [error, setError] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
+    const [weather, setWeather] = useState({})
 
-                <h1>Weather App</h1>
-                <p>31-08-2023</p>
+    const api = {
+        url: "https://api.openweathermap.org/data/2.5/",
+        key: "664384e76601b0d747d1884a39fb08bc",
+    }
+    
+    return (
+        <section className='--100vh --center-all'>
+            <div className="container weather --flex-center">
 
-                <div className="--form-control --my2">
-                    <input type="text" placeholder='Search city name' />
-                </div>
+                <div className="weather-app --text-light">
 
-                <div className="result --card --my2">
-                    <h2>Abuja</h2>
-                    <div className="icon">
-                        <img src="" alt="Clouds" />
+                    <h1>Weather App</h1>
+                    <p>31-08-2023</p>
+
+                    <div className="--form-control --my2">
+                        <input type="text" placeholder='Search city name' />
                     </div>
 
-                    <p>Temp: 23°c</p>
-                    <p>Weather: Clouds</p>
-                    <p>Temp range: 23°c / 24°C</p>
+                    <div className="result --card --my2">
+                        <h2>Abuja</h2>
+                        <div className="icon">
+                            <img src="" alt="Clouds" />
+                        </div>
 
-                </div>
-            </div> 
-        </div>
-    </section>
-  )
+                        <p>Temp: 23°c</p>
+                        <p>Weather: Clouds</p>
+                        <p>Temp range: 23°c / 24°C</p>
+
+                    </div>
+                </div> 
+            </div>
+        </section>
+    )
 }
 
 export default Weather
